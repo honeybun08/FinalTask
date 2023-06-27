14f721d78e2b136c778c43ebb7b599dd80643e5a
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-27 20:14:34 +0300

    An almost done task

---
+++ Task/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+
+void NewArrayString(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+
+
+NewArrayString(array1, array2);
+Console.Write("[");
+PrintArray(array2);
+Console.WriteLine("]");
+
+
+
+
